5d4e064c27e40f7ec3a0678e681c3cebef64a4fb
John Doe <johndoe@example.com>
2025-06-13 10:45:27 -0400

    Add package-lock.json with initial dependencies for colors and moment

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("Hello, World!");
+
+let name = "edgar";
+const age = 30;
+var apellido = "gonzalez";
+let sueldo = 1000.50;
+
+let carreras = {
+    tsu: "Tecnico Superior Universitario",
+    ing: "Ingeniero",
+    maestria: "Maestria en Ciencias de la Computacion",
+    doctorado: "Doctorado en Ciencias de la Computacion"
+}
+
+console.log(`Hola, mi nombre es ${name} y tengo ${age} años. Mi apellido es ${apellido} y mi sueldo es ${sueldo}.`);
